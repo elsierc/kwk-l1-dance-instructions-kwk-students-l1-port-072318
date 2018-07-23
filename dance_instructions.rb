f1fe403f8a0c4fb64f88c51745d0a4e31181ce54
elsierc <elsierc22@gmail.com>
2018-07-23 23:36:41 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,5 +1,51 @@
-def lower_body
+def assume_stance 
 puts "Plant legs far apart, bend knees slightly and keep posture loose."
+
+def footwork
+  puts "lift right foot."
+  puts "return right foot to the ground."
+  puts "finishing with a small skip-step backward."
+end 
+#footwork
+
+def skip-step
+  puts "Lower left foot to the ground"
+  puts "Bounce left foot back up slightly, kicking a few inches back"
+  puts "Left, right, left and left"
+  puts "Lower right foot to ground"
+  puts "Bounce right foot back up slight, kicking a few inches back"
+  puts "Right, left, right and right"
+end 
+#skip-step
+
+def bob_the_reins
+  puts "holding your arms out in front of you, straight and at chest level."
+  puts "cross your right wrist over your left and hold them together"
+  puts "lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end 
+#bob_the_reins
+
+def lasso 
+  puts "start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left"
+  puts "lift right arm up and point right elbow diagnoly to the right"
+  puts "raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso."
+  puts "this movement is repeated eight times"
+end 
+#lasso 
+
+def bust_a_move 
+  puts "be cool"
+  puts "be snazzy"
+end 
+bust_a_move
+
+puts "Gangnam_Style"
+assume_stance
+bust_a_move
+footwork
+bob
+
+
 
 
   
